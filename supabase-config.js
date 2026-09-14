268801428eb27bbe391e0890f82f89f51db71fa9
emilyim0709 <emilyimemilyim@gmail.com>
2026-09-14 11:31:53 +0900

    Add files via upload

---
+++ supabase-config.js
@@ -0,0 +1,6 @@
+// Supabase 대시보드의 Project URL과 Publishable key(또는 anon key)를 입력하세요.
+// service_role 키는 절대 입력하지 마세요.
+window.HAPPY_CAFE_SUPABASE = {
+  url: 'https://gmvrbwqmuvuzqmjqowa.supabase.co',
+  anonKey: 'sb_publishable_6zjqauxzqpDF06CJAUwgMg_YB8MDbXa'
+};
